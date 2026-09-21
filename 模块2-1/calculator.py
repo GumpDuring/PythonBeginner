d1493d5fb27c4f8f76de3e3ad6b6ac2cafa7b972
Gump <G15979601676@163.com>
2026-09-21 20:45:33 +0800

    A brand new version of command line calculator!Neater code expresion.More engineered.

--- 模块2-1/calculator.py
+++ 模块2-1/calculator.py
@@ -1,51 +1,49 @@
-def calculator():
-    print("_"*30)
-    print("欢迎使用命令行计算器")
-    print("_"*30)
-    #获取第一个数
-    try:
-        num1 = float(input("请输入第一个数字:").strip())
-    except ValueError:
-        print("请输入正确的数字！")
-        return
-    #获取计算符
-    print("请输入运算符(+ - * /):")
-    operator = input().strip()
-    if operator not in ['+','-','*','/']:
-        print("运算符错误！")
-        return
-    #获取第二个数
-    try:
-        num2 = float(input("请输入第二个数字:").strip())
-    except ValueError:
-        print("请输入正确的数字！")
-        return
-    #计算
-    if operator == "+":
-        result = num1 + num2
-    elif operator == "-":
-        result = num1 - num2
-    elif operator == "*":
-        result = num1 * num2
-    elif operator == "/":
-        if num2 == 0:
-            print("零不能做除数！")
-            return
-        else:
-            result = num1 / num2
-    print(f"计算结果为：{num1} {operator} {num2} = {round(result,2)}")
-    if abs(result - int(result)) <= 1e-10:
-        print("这是一个整数")
-    else:
-        print("这是一个小数")
-#程序入口
-if __name__ == "__main__":
-    calculator()
+def get_number(prompt):
     while True:
-        again = input("是否继续计算？(y/n):").strip().lower()
-        if again == "y":
-            calculator()
-        elif again  == "n":
-            break
+        try:
+            return float(input(prompt).strip())
+        except ValueError:
+            print("请输入正确的数字！")
+
+def gain():
+    while True:
+        mode = input("请选择模式:(+,-,*,/)").strip()
+        if mode not in ['+', '-','*','/']:
+            print('请正确输入运算符！')
         else:
-            print("请输入y或n！！")
+            break
+    num1 = get_number("请输入第一个数！")
+    num2 = get_number("请输入第二个数！")
+    return mode,num1,num2
+
+def calculate(mode,num1,num2):
+    if mode == "+":
+        result = num1 +num2
+        return result
+    elif mode == "-":
+        result = num1 - num2
+        return result
+    elif mode == "*":
+        result = num1 * num2
+        return result
+    elif mode == "/":
+        if num2 == 0:
+            return "Error!0不能作为除数!"
+        else:
+            result = num1 /num2
+            return result
+
+def main():
+    print('-'*30)
+    print('欢迎使用命令行计算器！')
+    print('-'*30)
+    while True:
+        mode,num1,num2 = gain()
+        result = calculate(mode,num1,num2)
+        print(f"输出:{result}")
+        again = input('计算结束，键入q以退出计算！').strip().lower()
+        if again == "q":
+            break
+
+if __name__ =="__main__":
+    main()
